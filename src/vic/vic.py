3838caaa6ea3002dfadf8d0f79ffb9da42603ec0
Nathan <healbadbad@yahoo.com>
2021-11-15 16:17:19 -0500

    Adding right click to inspect attributes

--- src/vic/vic.py
+++ src/vic/vic.py
@@ -115,14 +115,20 @@ def interact(locals, do_traceback=False, ignore_builtins=False, show_tensors=Fal
         props, funs = get_attributes_list(focused_object_name)
         locals_layout = [[sg.Column(modules, key="modules-column"), sg.Column(local_vars, key="local-vars-column"), sg.Column(functions, key='functions-column'),]]
         locals_column = sg.Frame("Local objects", [[sg.Column(locals_layout, scrollable=True, expand_x=True, expand_y=True)]], size=(80,30), expand_x=True, expand_y=True )
-        attributes_listbox = sg.Listbox(props, size=(80, 30), key='attributes')
+
+
+        attributes_right_click_menu = ['Attr', ['inspect-attribute']]
+        attributes_listbox = sg.Listbox(props, size=(80, 30), key='attributes', right_click_menu=attributes_right_click_menu)
 
         functions_listbox_size = (50, 30)
         functions_listbox = sg.Listbox(funs, size=functions_listbox_size, key=functions_list_key, enable_events=True)
+
         docstring_textbox_size = (80, detailed_info_height)
         docstring_textbox = sg.Multiline("docstring will be displayed here", size=docstring_textbox_size, key='docstring')
 
-        var_value = sg.Multiline("Variable value", size=(90, detailed_info_height), key='var_value')
+        var_val_str = focused_object_name
+        var_value = sg.Multiline(var_val_str, size=(90, detailed_info_height), key='var_value')
+        #TODO set variable info here
         variable_frame = sg.Frame('Variable Info', [[var_value]], key='var_name')
         docstring_box = sg.Frame('Docstring',[[docstring_textbox]])
 
@@ -130,16 +136,20 @@ def interact(locals, do_traceback=False, ignore_builtins=False, show_tensors=Fal
         execute_button = sg.Button('Execute', key='execute-button')
         builtins_checkbox = sg.Checkbox("Show Builtins", not ignore_builtins, key="show-builtins", enable_events=True)
         show_tensors_checkbox = sg.Checkbox("Show Tensors", show_tensors, key="show-tensors", enable_events=True)
+
+
         layout = [ [sg.Button("EXIT"), sg.Text('Locals'), builtins_checkbox, show_tensors_checkbox, interact_textbox, execute_button],
             [sg.Text('Modules'), sg.Text('Objects'), sg.Text('Callables')],
             [locals_column, attributes_listbox, functions_listbox],
             [variable_frame, docstring_box],
                 ]
+
         return layout
 
     #  minlen, max()
     detailed_info_height = 20
     focused_object_name = now_locals_names[-1]
+    focused_object_path = [focused_object_name]
 
     layout = generate_layout(focused_object_name, ignore_builtins, show_tensors)
 
@@ -166,18 +176,28 @@ def interact(locals, do_traceback=False, ignore_builtins=False, show_tensors=Fal
         # inspect a function that was clicked on
         if event == functions_list_key: 
             selected_function_name = values[functions_list_key][0].split(':')[0]
-            docstr = getattr(locals[focused_object_name], selected_function_name).__doc__
+            # attribute_name = values['attributes'][0].split(':')[0]
+            focused_object = locals[focused_object_path[0]]
+            path_str = focused_object_path[0]
+            if len(focused_object_path) > 1:
+                for attr_name in focused_object_path[1:]:
+                    focused_object = getattr(focused_object, attr_name)
+                    path_str = f"{path_str}.{attr_name}"
+
+            #docstr = getattr(locals[focused_object_name], selected_function_name).__doc__
+            docstr = getattr(focused_object, selected_function_name).__doc__
             if docstr is None or docstr == "":
                 docstr = f"No Docstring avalable for {selected_function_name}"
 
 
             # Retrieve function signature
             try:
-                sig = inspect.signature(getattr(locals[focused_object_name], selected_function_name))
+                #sig = inspect.signature(getattr(locals[focused_object_name], selected_function_name))
+                sig = inspect.signature(getattr(focused_object, selected_function_name))
             except:
-                sig = " -- Unable to retrieve signatrue "
+                sig = " -- Unable to retrieve signature "
 
-            displaystr = f"{focused_object_name}.{selected_function_name}" + str(sig) + "\n\n" + str(docstr)
+            displaystr = f"{path_str}.{selected_function_name}" + str(sig) + "\n\n" + str(docstr)
 
             window['docstring'].update(displaystr)
 
@@ -209,50 +229,97 @@ def interact(locals, do_traceback=False, ignore_builtins=False, show_tensors=Fal
                 print("Exception {Exception}")
                 print(e)
             continue
+        if event == "inspect-attribute":
+            # focused_object_name = event
+            print("start focused_object_path: ", focused_object_path)
+            if len(values['attributes']) < 1:
+                print("No attribute selected")
+                continue
+            attribute_name = values['attributes'][0].split(':')[0]
+            focused_object_path.append(attribute_name)
+            print("end focused_object_path: ", focused_object_path)
+            focused_object = locals[focused_object_path[0]]
+            path_str = focused_object_path[0]
+            if len(focused_object_path) > 1:
+                for attr_name in focused_object_path[1:]:
+                    focused_object = getattr(focused_object, attr_name)
+                    path_str = f"{path_str}.{attr_name}"
 
+            # Only update These if the buttons were selected
+            attributes_list, functions = get_attributes_list(focused_object)
+            window['attributes'].update(attributes_list)
+            window['functions_list'].update(functions)
+
+            #window['var_name'].update(f"{focused_object_name}.{attribute}")
+            window['var_name'].update(path_str)
+
+            var_val_str = str(focused_object)
+            window['var_value'].update(var_val_str)
+            continue
+            
         else:
             focused_object_name = event
+            focused_object_path = [focused_object_name]
+            # Only update These if the buttons were selected
+            attributes_list, functions = get_attributes_list(locals[focused_object_name])
+            window['attributes'].update(attributes_list)
+            window['functions_list'].update(functions)
+            window['var_name'].update(focused_object_name)
+            var_val_str = str(locals[focused_object_name])
+            window['var_value'].update(var_val_str)
+        
 
 
-        # Only update These if the buttons were selected
-        attributes_list, functions = get_attributes_list(locals[event])
-        window['attributes'].update(attributes_list)
-        window['functions_list'].update(functions)
-        window['var_name'].update(event)
-        var_val_str = str(locals[event])
-        window['var_value'].update(var_val_str)
 
 
         # Import cv2 or matplotlib to show and display tensors 
         # that can be formatted as gray or RGB images
         if show_tensors:
             if hasattr(locals[event], "T"):
+                # Is a torch or numpy array
+                displayable = False
+                is_torch_T = False
+                if hasattr(locals[event], "device"):
+                    is_torch_T = True
+
                 tensor = locals[event]
                 print(f"tensor: {event}")
                 # is a torch Tensor
                 # Check shape
+                to_display = None
                 if len(tensor.shape) == 2:
                     aspect_ratio = max(tensor.shape) / min(tensor.shape)
                     print(f"aspect ratio: {aspect_ratio}")
                     if aspect_ratio < 10:
                         print("probably reasonable to display")
-                        import cv2
                         if max(tensor.shape) < 352:
                             scale_factor = 352 / max(tensor.shape)
                             newshape = (int(tensor.shape[0] *scale_factor), int(tensor.shape[1] * scale_factor))
                             resized = cv2.resize(tensor.unsqueeze(-1).cpu().detach().numpy(), newshape, interpolation=cv2.INTER_NEAREST)
-                            cv2.imshow(f'Tensor-{event}', resized)
+                            # cv2.imshow(f'Tensor-{event}', resized)
+                            to_display = resized
                         else:
-                            cv2.imshow(f'Tensor-{event}', tensor.unsqueeze(-1).cpu().detach().numpy())
+                            to_display = tensor.unsqueeze(-1).cpu().detach().numpy()
+                        displayable = True
+
+                            # cv2.imshow(f'Tensor-{event}', tensor.unsqueeze(-1).cpu().detach().numpy())
                     #TODO: Display if enabled
 
-                if len(tensor.shape) >= 2:
+                elif len(tensor.shape) >= 2:
                     print("at least two dimensional")
                     if 1 in tensor.shape or 3 in tensor.shape:
                         print("correct # of channels")
                         if tensor.shape[1] == 3 or tensor.shape[1] == 1:
-                            import cv2
-                            cv2.imshow(f'Tensor-{event}', tensor.squeeze(0).permute((1,2,0)).cpu().detach().numpy())
+                            #cv2.imshow(f'Tensor-{event}', tensor.squeeze(0).permute((1,2,0)).cpu().detach().numpy())
+                            to_display = tensor.squeeze(0).permute((1,2,0)).cpu().detach().numpy()
+                            displayable = True
+                else:
+                    continue
+                
+                # Select display method
+                if True and displayable: 
+                    import cv2
+                    cv2.imshow(f'Tensor-{event}', to_display)
 
                 print("")
 
